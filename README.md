bd6ad9300c9d11f92978679ff1dc7535d5dc4358
GabrielHoffman <gabrielhoffman@typhon.fios-router.home>
2020-03-22 21:12:33 -0400

    add new annotations

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# cmc_analysis
-cmc_analysis
+# CMC v3 analysis
+
+
+### GENCODE v30 
+
++ GTF [syn21814507](https://www.synapse.org/#!Synapse:syn21814507)
++ Gene annotations [syn21814510](https://www.synapse.org/#!Synapse:syn21814510)
++ Transcript annotations [syn21814517](https://www.synapse.org/#!Synapse:syn21814517)
+
+### Gene sets
++ more coming soon
+
+mSigDB v7.0: [syn21814525](https://www.synapse.org/#!Synapse:syn21814525)
+
+```r
+# Read GMT into list with
+# BiocManager::install("qusage")
+library(qusage)
+mSigDB_list = read.gmt( "msigdb.v7.0.symbols.gmt" )
+```
+
--- src/get_gene_annotations.R
+++ src/get_gene_annotations.R
@@ -24,14 +24,15 @@ setkey(df_gene, "gene_id")
 
 ensGenes = unique( df_gene$gene_id )
 
-res_gene = lapply( ensGenes[1:10], function(ensGene){
+res_gene = lapply( ensGenes, function(ensGene){
 	df_gene[gene_id==ensGene,data.frame(seqnames, start, end, strand, type, gene_id, gene_name, gene_type)]
 })
-res_gene = do.call("rbind", res_gene)
+res_gene = data.table(do.call("rbind", res_gene))
 
-write.table(res_gene, file=gzfile("gencode_v30_gene_annoation.tsv.gz"), row.names=FALSE, sep="\t", quote=FALSE)
+res_gene[,TSS:=ifelse(strand=='+', start, end)]
+res_gene[,TES:=ifelse(strand=='+', end, start)]
 
-
+write.table(res_gene, file=gzfile("gencode_v30_gene_annotation.tsv.gz"), row.names=FALSE, sep="\t", quote=FALSE)
 
 # Transcript
 ############
@@ -41,12 +42,15 @@ setkey(df_tr, "gene_id")
 
 ensGenes = unique( df_tr$gene_id )
 
-res_tr = lapply( ensGenes[1:10], function(ensGene){
+res_tr = lapply( ensGenes, function(ensGene){
 	df_tr[gene_id==ensGene,data.frame(seqnames, start, end, strand, type, gene_id, gene_name, gene_type, transcript_id, transcript_type)]
 })
 res_tr = do.call("rbind", res_tr)
 
-write.table(res_tr, file=gzfile("gencode_v30_transcript_annoation.tsv.gz"), row.names=FALSE, sep="\t", quote=FALSE)
+res_tr[,TSS:=ifelse(strand=='+', start, end)]
+res_tr[,TES:=ifelse(strand=='+', end, start)]
+
+write.table(res_tr, file=gzfile("gencode_v30_transcript_annotation.tsv.gz"), row.names=FALSE, sep="\t", quote=FALSE)
 
 
 
@@ -54,22 +58,38 @@ write.table(res_tr, file=gzfile("gencode_v30_transcript_annoation.tsv.gz"), row.
 # Upload Synapse results
 ########################
 
- # Adding files with provenance:
- #
- # A synapse entity *syn1906480* contains data
- # entity *syn1917825* contains code
- #
- activity <- Activity(
-     'GENCODE 30 gene annotation',
-     description='GENCODE 30 gene annotation',
-     used=c('syn1906480', 'http://data_r_us.com/fancy/data.txt'),
-     executed='syn1917825')
- file <- File('gencode_v30_gene_annoation.tsv.gz', description='GENCODE 30 gene annotation', parent=project)
- file <- synStore(file, activity=activity)
+# gene
+activity <- Activity(
+ 'GENCODE 30 gene annotation',
+ description='GENCODE 30 gene annotation',
+ used=c('syn21814507'),
+ executed='https://github.com/GabrielHoffman/cmc_analysis/blob/master/src/get_gene_annotations.R')
+file <- File('gencode_v30_gene_annotation.tsv.gz', description='GENCODE 30 gene annotation', parent='syn21814506')
+file <- synStore(file, activity=activity)
 
 
+# transcript
+activity <- Activity(
+ 'GENCODE 30 transcript annotation',
+ description='GENCODE 30 transcript annotation',
+ used=c('syn21814507'),
+ executed='https://github.com/GabrielHoffman/cmc_analysis/blob/master/src/get_gene_annotations.R')
+file <- File('gencode_v30_transcript_annotation.tsv.gz', description='GENCODE 30 transcript annotation', parent='syn21814506')
+file <- synStore(file, activity=activity)
 
 
+# library(xlsx)
+
+# df = read.xlsx("~/Downloads/GREX_PrediXcan_TWAS_SMR_GeneList_11.04.19.xlsx", 1)
+# df$DISORDER = as.character(df$DISORDER)
+# df$Gene = as.character(df$Gene)
+
+# ids =  unique(gsub(' ', '', unlist(strsplit(ids, ','))))
+
+# res_list = lapply(ids, function(id){
+# 	df[grep(id,df$DISORDER),1]
+# 	})
+# names(res_list) = ids
 
 
 
